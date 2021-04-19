d0e0c4040f080450b5db68b81cb0f79201f3010b
Mathis <mathisouchikh@gmail.com>
2021-04-19 16:03:48 +0200

    id et suppression href header

--- php/partials/header.php
+++ php/partials/header.php
@@ -14,8 +14,8 @@ function partials_header($page){
     <header>
         <div class="container-fluid"  style="border-bottom: 1px solid #1c55a3; margin-top: 15px;">
             <div class="row">
-                <div class="col-sm-2"><a href="../index.html"><img src="assets/img/logo.png" width="45%" height="90%"></a></div>
-                <div class="col-sm-9"><a href="../index.html"><img src="assets/img/header.png" width="95%"></a></div>
+                <div class="col-sm-2"><a href=""><img src="assets/img/logo.png" width="45%" height="90%"></a></div>
+                <div class="col-sm-9"><a href"><img src="assets/img/header.png" width="95%"></a></div>
             </div>
         </div>
     </header>
@@ -23,64 +23,64 @@ function partials_header($page){
     <nav class="navbar navbar-expand-sm bg-white justify-content-center sticky-top">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="../index.html">Accueil</a>
+                <a class="nav-link" href="">Accueil</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">La fédération</a>
+                <a class="nav-link dropdown-toggle" href="#" id="nav-federation" data-toggle="dropdown">La fédération</a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="../federation/federation.html">La fédération</a>
-                    <a class="dropdown-item" href="../federation/calendrier.html">Calendrier</a>
-                    <a class="dropdown-item" href="../federation/map.html">Les clubs</a>
-                    <a class="dropdown-item" href="../federation/contacts.html">Contacts</a>
-                    <a class="dropdown-item" href="../federation/lien.html">Lien</a>
-                    <a class="dropdown-item" href="../federation/legal.html">Label-Légal-régularité</a>
+                    <a class="dropdown-item" href="" id="federation" >La fédération</a>
+                    <a class="dropdown-item" href="" id="calendrier">Calendrier</a>
+                    <a class="dropdown-item" href="" id="clubs">Les clubs</a>
+                    <a class="dropdown-item" href="" id="contact-federation">Contacts</a>
+                    <a class="dropdown-item" href="" id="lien">Lien</a>
+                    <a class="dropdown-item" href="" id="label">Label-Légal-régularité</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Direction technique</a>
+                <a class="nav-link dropdown-toggle" href="#" id="nav-dir-tech" data-toggle="dropdown">Direction technique</a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="../direction_tech/conseilMaitre.html">Le conseil des ME</a>
-                    <a class="dropdown-item" href="../direction_tech/listeMaitre.html">Liste O.CN et ME</a>
-                    <a class="dropdown-item" href="../direction_tech/calendrier.html">Calendrier technique</a>
-                    <a class="dropdown-item" href="../direction_tech/stage.html">Stages GRT Coupes</a>
-                    <a class="dropdown-item" href="../direction_tech/changeGrade.html">Passage de grades</a>
-                    <a class="dropdown-item" href="../direction_tech/formation.html">Formations</a>
+                    <a class="dropdown-item" href="" id="conseil-ME">Le conseil des ME</a>
+                    <a class="dropdown-item" href="" id="liste-ME">Liste O.CN et ME</a>
+                    <a class="dropdown-item" href="" id="calendrier-tech">Calendrier technique</a>
+                    <a class="dropdown-item" href="" id="stage-GRT">Stages GRT Coupes</a>
+                    <a class="dropdown-item" href="" id="grades">Passage de grades</a>
+                    <a class="dropdown-item" href="" id="formation">Formations</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Le Vovinam VVD</a>
+                <a class="nav-link dropdown-toggle" href="#" id="nav-vovinam" data-toggle="dropdown">Le Vovinam VVD</a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="../le-viet-vo-dao/la-discipline.html">Le sport et ses valeurs</a>
-                    <a class="dropdown-item" href="../le-viet-vo-dao/l-histoire.html">Son histoire</a>
-                    <a class="dropdown-item" href="../le-viet-vo-dao/la-fede-mondiale.html">La fédération mondiale</a>
-                    <a class="dropdown-item" href="../le-viet-vo-dao/les-grands-ME.html">Les grands Maîtres</a>
+                    <a class="dropdown-item" href="" id="valeurs">Le sport et ses valeurs</a>
+                    <a class="dropdown-item" href="" id="histoires">Son histoire</a>
+                    <a class="dropdown-item" href="" id="federation-mondiale">La fédération mondiale</a>
+                    <a class="dropdown-item" href="" id="grands-maitres">Les grands Maîtres</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Affiliation/licenciés</a>
+                <a class="nav-link dropdown-toggle" href="#" id="nav-licence" data-toggle="dropdown">Affiliation/licenciés</a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="../Affiliation-licenciés/documentation.html">Documentation</a>
-                    <a class="dropdown-item" href="../Affiliation-licenciés/modalités_affiliation.html">modalités d'Affiliation</a>
-                    <a class="dropdown-item" href="../Affiliation-licenciés/faq_affiliation.html">FAQ Affiliation</a>
-                    <a class="dropdown-item" href="../Affiliation-licenciés/licenciés.html">Licenciés</a>
-                    <a class="dropdown-item" href="../Affiliation-licenciés/passeports.html">Passeport</a>
+                    <a class="dropdown-item" href="" id="documentation">Documentation</a>
+                    <a class="dropdown-item" href="" id="mod-affiliation">modalités d'Affiliation</a>
+                    <a class="dropdown-item" href="" id="FAQ-affiliation">FAQ Affiliation</a>
+                    <a class="dropdown-item" href="" id="licences">Licenciés</a>
+                    <a class="dropdown-item" href="" id="passeport">Passeport</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Actualités</a>
+                <a class="nav-link dropdown-toggle" href="#" id="nav-actualites" data-toggle="dropdown">Actualités</a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="../Actualités/informations.html">Informations</a>
-                    <a class="dropdown-item" href="../Actualités/msg_regional.html">Message Régional</a>
-                    <a class="dropdown-item" href="../Actualités/msg_national.html">Message National</a>
-                    <a class="dropdown-item" href="../Actualités/msg_internanional.html">Message International</a>
+                    <a class="dropdown-item" href="" id="informations">Informations</a>
+                    <a class="dropdown-item" href="" id="msg-regional">Message Régional</a>
+                    <a class="dropdown-item" href="" id="msg-national">Message National</a>
+                    <a class="dropdown-item" href="" id="msg-international">Message International</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Contacts</a>
+                <a class="nav-link dropdown-toggle" href="#" id="nav-contact" data-toggle="dropdown">Contacts</a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href="../Contacts/contacts.html">Contacts</a>
-                    <a class="dropdown-item" href="../Contacts/faq.html">FAQ</a>
-                    <a class="dropdown-item" href="../Contacts/personnalite-federation.html">Personnalité de la fédération</a>
+                    <a class="dropdown-item" href="" id="contact-contact">Contacts</a>
+                    <a class="dropdown-item" href="" id="FAQ">FAQ</a>
+                    <a class="dropdown-item" href="" id="personnalite">Personnalité de la fédération</a>
                 </div>
             </li>
         </ul>
