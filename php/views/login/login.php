93e3f4f6e54ee63703506e900f2e588f365f4c3d
Mathis <mathisouchikh@gmail.com>
2021-04-21 15:38:48 +0200

    Page de connexion

--- php/views/login/login.php
+++ php/views/login/login.php
@@ -1,22 +1,32 @@
-<div id="container">
-            <!-- zone de connexion -->
-            
-            <form action="verification.php" method="POST">
-                <h1>Connexion</h1>
-                
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/federation.css">
+    <title>Connexion</title>
+</head>
 
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
-                <input type="submit" id='submit' value='LOGIN' >
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-                ?>
-            </form>
+<div id="logine">
+    <form action="verification.php" method="POST">
+    <div class="login">
+        <h1>Connexion</h1>
+        <div class ="user">
+            <label><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
         </div>
+        <div class ="password">
+            <label><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+        </div>
+            <div class ="submit">
+            <input type="submit" id='submit' value='LOGIN' >
+        </div>
+        <?php
+            if(isset($_GET['erreur'])){
+            $err = $_GET['erreur'];
+            if($err==1 || $err==2)
+            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+        }
+        ?>
+    </div>
+    </form>
+</div>
