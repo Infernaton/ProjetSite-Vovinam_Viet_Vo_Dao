66584a082deffd4d1e035f2350eb3ce6c87f96d0
Mathis <mathisouchikh@gmail.com>
2021-04-29 17:07:56 +0200

    Panel admin update

--- php/views/admin/admin.php
+++ php/views/admin/admin.php
@@ -1,6 +1,4 @@
-<header> 
-    
-</header>
+<link rel="stylesheet" href="css/add.css">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -8,10 +6,15 @@
 
 <div id="container" class="container">
     <h1 id="panel">Panel adminnistateur</h1>
-    
-    
-    <input type=button class="btn-redirect" onClick="parent.open('?c=admin&p=1')" value='Ajouter un Grand Maître' >
-    <input type=button class="btn-redirect" onClick="parent.open('?c=admin&p=2')" value='Ajouter un club' >
+        <div class="btn-redirect">
+            <a href="?c=admin&p=1">
+                <button>Ajouter un Grand Maître</button>
+            </a>
+
+            <a href="?c=admin&p=2">
+                <button>Ajouter un club</button>
+            </a>
+        </div>
 
 
 
