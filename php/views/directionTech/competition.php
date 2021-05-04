2393a9845922fc2afd5e9b6048cc5b0101a4e743
Mathis <mathisouchikh@gmail.com>
2021-05-04 15:16:32 +0200

    Update competition.php

--- php/views/directionTech/competition.php
+++ php/views/directionTech/competition.php
@@ -6,10 +6,8 @@
     
     <!--Drawer for all season-->
     <div class="inline">
-        <h4><b>Saison</b></h4>
-        <a class="btn btn-outline-secondary" onclick="multiCollapseButton('season', 'first')">20XX-2015</a>
-        <a class="btn btn-outline-secondary" onclick="multiCollapseButton('season', 'second')">2015-2020</a>
-        <a class="btn btn-outline-secondary" onclick="multiCollapseButton('season', 'third')">2020-Aujourd'hui</a>
+        <a class="btn btn-outline-secondary" onclick="multiCollapseButton('season', 'first')">A venir</a>
+        <a class="btn btn-outline-secondary" onclick="multiCollapseButton('season', 'second')">Précédente compétitions</a>
             <!--Apply the information from the button we push-->
     </div>
     <div class="container pt-2 my-3 border">
@@ -17,17 +15,12 @@
         </div>
         <div class="hide">
             <div id="first">
-                <h5>20XX-2015</h5>
+                <h5>Compétitions à venir</h5>
                 <p>-LOREM IPSUM-</p>
                 <br>
             </div>
             <div id="second">
-                <h5>2015-2020</h5>
-                <p>-LOREM IPSUM-</p>
-                <br>
-            </div>
-            <div id="third">
-                <h5>2020-Aujourd'hui</h5>
+                <h5>Précédente compétitions</h5>
                 <p>-LOREM IPSUM-</p>
                 <br>
             </div>
