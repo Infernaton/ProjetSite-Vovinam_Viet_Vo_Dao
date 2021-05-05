365303fa0909076377a79f063b28eb5c3b95540a
Infernaton <lubin.pauly@gmail.com>
2021-05-05 12:59:33 +0200

    reposition du addMaster

--- php/management/addMasterDB.php
+++ php/management/addMasterDB.php
@@ -4,13 +4,13 @@ require_once "../init.php";
 //Get the data from the url
 $url = parse_url($_SERVER['REQUEST_URI']);
 parse_str($url["query"],$result);
-var_dump($result);
+//var_dump($result);
 
 $result["death"] == null? $deathValue = "---" : $deathValue = $result["death"];
 
 //Prepare to add the object
 $req = $db->prepare('INSERT INTO specialist (
-    name, image, biography, birthday, deathDate, function
+    name, image, biographyShort, birthday, deathDate, function
     ) VALUES (:name, :image, :biography, :birthday, :deathDate, :function)');
 
 $req->bindValue(':name' , $result["name"]); 
@@ -22,7 +22,7 @@ $req->bindValue(':function' , $result["function"]);
 
 $req->execute();
 
-echo "\n Envoie réussi";
-//echo "<script type='text/javascript'> history.go(-1); </script>";
+//echo "\n Envoie réussi";
+echo "<script type='text/javascript'> history.go(-1); </script>";
 die;
 ?>
--- php/views/admin/addMaster.php
+++ php/views/admin/addMaster.php
@@ -1,53 +1,54 @@
 <link rel="stylesheet" href="css/add1.css">
 
-  <form action="php/management/addMasterDB.php">
-    <div id="container" class="container">
-      
-        <h1 id="addGM" class="content-title-red" >Ajout un Grand Maître à la Galerie</h1>
-        <div class="row" id="master-row">
-        <div class ="content">
-        
-          <div class = "nom">
-            <label class="data" for="name"><b>Nom</b></label>
-            <input class="inputData" type="text" placeholder="Nom" name="name" id="name" required>
-          </div>
-
-          <div class = "img">
-            <label class="data" for="image"><b>Lien Image</b></label>
-            <input class="inputData" type="text" placeholder="Lien d'une Image" name="image" id="image" required>
-          </div>
-
-          <div class = "bio">
-            <label class="data" for="biography"><b>Biographie</b></label>
-            <textarea class="inputData" rows="4" cols="50" placeholder="Biographie.." name="biography" id="biography" required></textarea>
-          </div>
-
-          <div class = "naiss">
-            <label class="data" for="birth"><b>Naissance</b></label>
-            <input class="inputData" type="text" placeholder="Naissance" name="birth" id="birth" required>
-          </div>
-
-          <div class = "deces">
-            <label class="data" for="tags"><b>Décès</b></label>
-            <input class="inputData" type="text" placeholder="Date du décès (optionnel)" name="death" id="death">
-          </div>
-
-          <div class = "fonction">
-            <label class="data" for="function"><b>Fonction(s) (séparées d'une virgule)</b></label>
-            <input class="inputData" type="text" placeholder="Liste des Fonctions qu'il a occupé durant sa vie" name="function" id="function">
-          </div>
+<form action="php/management/addMasterDB.php">
+  <div id="container" class="container">
+    <div class="text-center">
+      <h1 id="addGM" class="content-title-red" >Ajout un Grand Maître à la Galerie</h1>
+    </div>
+    <div class="row" id="master-row">
+      <div class="content col-sm-5">
+        <div class = "nom">
+          <label class="data" for="name"><b>Nom Complet</b></label>
+          <input class="inputData" type="text" placeholder="Nom" name="name" id="name" required>
         </div>
-        </div>
-          <div class="d-flex justify-content-between mb-3">
-            <div id="btn-object">
-                <a onclick="history.go(-1);"><button class="btn-annul annim" type="button" id='undo'>Annuler</button></a>
-            </div>
 
-            <div id="btn-Action">
-                <button type="submit" class="btn-modObject annim" value="valide" name="submit" id="confirm">Valider</button>
-            </div>
-          </div>
-        
+        <div class = "img">
+          <label class="data" for="image"><b>Photo</b></label>
+          <input class="inputData" type="text" placeholder="Lien d'une Image" name="image" id="image" required>
+        </div>
+
+        <div class = "naiss">
+          <label class="data" for="birth"><b>Date de Naissance</b></label>
+          <input class="inputData" type="text" placeholder="Naissance" name="birth" id="birth" required>
+        </div>
+
+        <div class = "deces">
+          <label class="data" for="tags"><b>Date du Décès</b></label>
+          <input class="inputData" type="text" placeholder="Décès (optionnel)" name="death" id="death">
+        </div>
+
+      </div>
+      <div class="content col-sm-7">
+
+        <div class = "bio">
+          <label class="data" for="biography"><b>Courte Biographie</b></label>
+          <textarea class="inputData" rows="4" cols="50" placeholder="Biographie.." name="biography" id="biography" required></textarea>
+        </div>
+
+        <div class = "fonction">
+          <label class="data" for="function"><b>Fonction(s) (séparées d'une virgule)</b></label>
+          <textarea class="inputData" cols="50" placeholder="Liste des Fonctions qu'il a occupé durant sa vie" name="function" id="function"></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex justify-content-between mb-3">
+      <div id="btn-object">
+        <a onclick="history.go(-1);"><button class="btn-annul annim" type="button" id='undo'>Annuler</button></a>
+      </div>
+
+      <div id="btn-Action">
+        <button type="submit" class="btn-modObject annim" value="valide" name="submit" id="confirm">Valider</button>
+      </div>
     </div>
   </div> 
 </form> 
