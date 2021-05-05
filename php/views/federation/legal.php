84734473e83296ede6b9f09266e27939e81844fe
Infernaton <lubin.pauly@gmail.com>
2021-05-05 18:34:42 +0200

    correction gros affichage

--- php/views/federation/legal.php
+++ php/views/federation/legal.php
@@ -2,8 +2,10 @@
 
 <div class="container info mt-5">
     <div class="text-center">
-        <h1 class="content-title-yellow text-center"> Infos Légales </h1>
+        <h1 class="content-title-yellow text-center"> Logo Légal </h1>
     </div>
+
+        <h3>Chartres</h3>
 
         <h3>Mentions Légales</h3>
         
