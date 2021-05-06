73a6f7f66f544b1e5b00976601a50892c2a3100e
Infernaton <lubin.pauly@gmail.com>
2021-05-06 17:53:03 +0200

    Début upload image pour les Maitres

--- php/management/addMasterDB.php
+++ php/management/addMasterDB.php
@@ -1,29 +1,62 @@
 <?php
 require_once "../init.php";
+/*
+var_dump($_POST);
+var_dump($_FILES);
+*/
+$target_dir = "../../assets/img/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-//Get the data from the url
-$url = parse_url($_SERVER['REQUEST_URI']);
-parse_str($url["query"],$result);
-//var_dump($result);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check file size
+if ($_FILES["image"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+        echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+$target_file = str_replace("../","",$target_file);
 
-$result["death"] == null? $deathValue = "---" : $deathValue = $result["death"];
+$_POST["death"] == null? $deathValue = "---" : $deathValue = $_POST["death"];
 
 //Prepare to add the object
 $req = $db->prepare('INSERT INTO specialist (
     name, pictureProfile, biographyShort, birthday, deathDate, pa_function, curr_function
     ) VALUES (:name, :image, :biography, :birthday, :deathDate, :pa_function, :curr_function)');
 
-$req->bindValue(':name' , $result["name"]);
-$req->bindValue(':image' , $result["image"]);
-$req->bindValue(':biography', $result["biography"]);
-$req->bindValue(':birthday' , $result["birth"]);
+$req->bindValue(':name' , $_POST["name"]);
+$req->bindValue(':image' , $target_file);
+$req->bindValue(':biography', $_POST["biography"]);
+$req->bindValue(':birthday' , $_POST["birth"]);
 $req->bindValue(':deathDate' , $deathValue);
-$req->bindValue(':pa_function' , $result["paFunction"]);
-$req->bindValue(':curr_function' , $result["currFunction"]);
+$req->bindValue(':pa_function' , $_POST["paFunction"]);
+$req->bindValue(':curr_function' , $_POST["currFunction"]);
 
-$req->execute();
+//$req->execute();
 
 //echo "\n Envoie réussi";
-echo "<script type='text/javascript'> history.go(-1); </script>";
+//echo "<script type='text/javascript'> history.go(-1); </script>";
 die;
+
 ?>
--- php/views/admin/addMaster.php
+++ php/views/admin/addMaster.php
@@ -1,9 +1,11 @@
+<?php var_dump(pathinfo(getcwd()));?>
+
 <link rel="stylesheet" href="css/add.css">
 
-<form action="php/management/addMasterDB.php">
-  <div id="container" class="container">
+<form action="php/management/addMasterDB.php" method="post" enctype="multipart/form-data">
+  <div id="container" class="container mt-5">
     <div class="text-center">
-      <h1 id="addGM" class="content-title-red" >Ajout un Grand Maître à la Galerie</h1>
+      <h1 id="addGM" class="content-title-red" >Ajouter un Maître</h1>
       <br><br>
       <h5> <span class="note">*</span> : Champs Obligatoire</h5>
     </div>
@@ -13,7 +15,7 @@
         <input class="inputData" type="text" placeholder="Nom" name="name" id="name" required>
 
         <label class="data" for="image"><b>Photo</b><span class="note">*</span></label>
-        <input class="inputData" type="text" placeholder="Lien d'une Image" name="image" id="image" required>
+        <input class="inputData" type="file" placeholder="Lien d'une Image" name="image" id="image" accept="image/png, image/jpeg" required>
 
         <label class="data" for="birth"><b>Date de Naissance</b><span class="note">*</span></label>
         <input class="inputData" type="text" placeholder="'jj/mm/aaaa' ou 'aaaa'" name="birth" id="birth" required>
@@ -24,22 +26,27 @@
         <label class="data" for="currFunction"><b>Fonction Actuelle</b><span class="note">*</span></label>
         <textarea class="inputData" cols="40" placeholder="Fonction occupé actuellement par la personne" name="currFunction" id="currFunction" required></textarea>
       </div>
+
       <div class="content col-sm-7">
         <label class="data" for="biography"><b>Courte Biographie</b></label>
         <textarea class="inputData" rows="6" cols="50" placeholder="(Optionnel)" name="biography" id="biography"></textarea>
 
         <label class="data" for="paFunction"><b>Ancienne Fonction(s) occupée(s) <br>(séparées d'une virgule, si plusieurs)</b></label>
         <textarea class="inputData" cols="50" placeholder="Optionnel si aucune fonction effectuée dans le passé" name="paFunction" id="paFunction"></textarea>
-      </div>
-    </div>
-    <div class="d-flex justify-content-between mb-3">
-      <div id="btn-object">
-        <a onclick="history.go(-1);"><button class="btn-annul annim" type="button" id='undo'>Annuler</button></a>
+        
+        <div class="d-flex justify-content-between mt-4">
+
+          <div id="btn-object">
+            <a onclick="history.go(-1);"><button class="btn-annul annim" type="button" id='undo'>Annuler</button></a>
+          </div>
+
+          <div id="btn-Action" style="margin-right: 5%;">
+            <button type="submit" class="btn-modObject annim" value="valid" name="submit" id="confirm">Valider</button>
+          </div>
+
+        </div>
       </div>
 
-      <div id="btn-Action">
-        <button type="submit" class="btn-modObject annim" value="valide" name="submit" id="confirm">Valider</button>
-      </div>
     </div>
   </div> 
 </form> 
