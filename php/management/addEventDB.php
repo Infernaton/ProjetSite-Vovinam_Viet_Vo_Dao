aa1292b0bf2691b4e981ca228674e00d5b271bee
Infernaton <lubin.pauly@gmail.com>
2021-05-14 17:17:35 +0200

    Modification des évenements

--- php/management/addEventDB.php
+++ php/management/addEventDB.php
@@ -1,31 +1,80 @@
 <?php
 require_once "../init.php";
 
-//Get the data from the url
-$url = parse_url($_SERVER['REQUEST_URI']);
-parse_str($url["query"],$result);
-var_dump($result);
+if ($_FILES['newImage']['error']==0){
+    //If there no new file, $_FILES['newImage']['error'] = 4;
+    $target_dir = "../../assets/img/events/";
+    $target_file = $target_dir . basename($_FILES["newImage"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["newImage"]["tmp_name"]);
+        if($check !== false) {
+            //echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    // Check file size
+    if ($_FILES["newImage"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["newImage"]["tmp_name"], $target_file)) {
+            //echo "The file ". htmlspecialchars( basename( $_FILES["newImage"]["name"])). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+    $target_file = str_replace("../","",$target_file);
+}else {
+    //If we want to stay with the current image
+    $target_file = $_POST['oldImage'];
+}
+
 //Just in case we have to stock a date with the form dd/mm/yyyy
-$result['debut'] = str_replace("-","/",$result['debut']);
-$result['fin'] = str_replace("-","/",$result['fin']);
+$_POST['debut'] = str_replace("-","/",$_POST['debut']);
+$_POST['fin'] = str_replace("-","/",$_POST['fin']);
 
-//Prepare to add the object
-$req = $db->prepare('INSERT INTO `event` (
-    type, title, description, dateDebut, dateFin, prerequis
-    ) VALUES (:type, :title, :description, :dateDebut, :dateFin, :prerequis)');
+//var_dump($_POST);
 
-$req->bindValue(':type' , 'Compétition'); 
-$req->bindValue(':title' , $result["title"]);
-$req->bindValue(':description', $result["description"]);
-$req->bindValue(':dateDebut' , $result["debut"]);
-$req->bindValue(':dateFin' , $result["fin"]);
-$req->bindValue(':prerequis' , "");
+switch ($_POST['submit']){
+    case 'valid':
+        //Prepare to add the object
+        $req = $db->prepare('INSERT INTO `event` (
+            type, title, description, dateDebut, dateFin, prerequis,image
+            ) VALUES (:type, :title, :description, :dateDebut, :dateFin, :prerequis, :image)');
 
+        $req->bindValue(':type' , $_POST["event"]); 
+        $req->bindValue(':title' , $_POST["title"]);
+        $req->bindValue(':description', $_POST["description"]);
+        $req->bindValue(':dateDebut' , $_POST["debut"]);
+        $req->bindValue(':dateFin' , $_POST["fin"]);
+        $req->bindValue(':prerequis' , $_POST["prerequis"]);
 
-var_dump($req);
-$req->execute();
+        $req->execute();
+        echo "\n Envoie réussi";
+        break;
 
-echo "\n Envoie réussi";
-echo "<script type='text/javascript'> history.go(-1); </script>";
+    case 'modify':
+        $request = 'UPDATE event SET'.' type="'. $_POST["event"].'", title="'.$_POST["title"].'", description="'.$_POST["description"].'", dateDebut="'.$_POST["debut"].'", dateFin="'.$_POST["fin"].'", prerequis="'.$_POST["prerequis"].'", image="'.$target_file.'" WHERE id='.(int)$_POST['currentEvent'].'';
+        $req = $db->prepare($request);
+        $req->execute();
+        break;
+
+    case 'delete':
+        break;
+}
+
+echo "<script type='text/javascript'> history.go(-2); </script>";
 die;
 ?>
--- php/views/admin/event.php
+++ php/views/admin/event.php
@@ -1,18 +1,48 @@
+<?php
+function reformatDate($dateStringDB){
+    return str_replace("/","-",$dateStringDB);
+}
+//Get the index of the current master in the url
+$currentEvent = null;
+if (isset($_GET['e'])) {
+  if ($_GET['e'] != null) {
+    $index = $_GET['e'];
+    $req = $db->query('SELECT * FROM event as s WHERE s.id = '.$index.'');
+    $currentEvent = $req->fetch(PDO::FETCH_ASSOC);
+    $currentEvent['description'] = str_replace(array("\r","\n"),array("","{n}"),$currentEvent['description']);
+  } else {
+    $index = -1;
+  }
+} else {
+  $index = -1;
+}
+?>
 <link rel="stylesheet" href="css/add.css">
-<form action="php/management/addEventDB.php">
+<form action="php/management/addEventDB.php" method="post" enctype="multipart/form-data">
     <div id="container" class="container mt-2 mt-md-5">
         <div class="text-center"> 
             <h1 id="addGM" class="content-title-red">Créer un nouvel évènement</h1>
         </div>
     <div class="row">
         <div class="col-12 col-md-4">
+            <div id="previewImgDiv" class="responsive" style="background-image:url(assets/img/no-picture.png);";>
+                <div class="hoverEle">
+                    <p class="center">Changer l'image</p>
+                </div>
+                <input class="hide" type="text" name="oldImage" id="oldImage">
+                <input class="inputData input-file" type="file" name="newImage" id="newImage" accept=".png, .jpeg, .jpg" required onchange="previewFile(this);">
+            </div>
             <label class="data" for="title"><b>Titre</b></label>
             <input class="inputData form-control" type="text" placeholder="Titre" name="title" id="title" required>
 
             <label class="data" for="debut"><b>Date de Début de l'évènement</b></label>
-            <input class="inputData form-control" type="date" placeholder="jj/mm/aaaa" name="debut" id="dateDebut"required>
+            <input class="inputData form-control" type="date" placeholder="jj/mm/aaaa" name="debut" id="dateDebut" required>
             <label class="data" for="fin"><b>Date de Fin de l'évènement</b></label>
-            <input class="inputData form-control" type="date" placeholder="jj/mm/aaaa" name="fin" id="dateFin"required>
+            <input class="inputData form-control" type="date" placeholder="jj/mm/aaaa" name="fin" id="dateFin" required>
+            
+        </div>
+
+        <div class="col-12 col-md-8">
             <div id="categoryContainer">
                 <label class="data" for="event"><b>Type d'évènement</b></label>
                 <input class="inputData form-control" list="list" type="text" placeholder="  -" name="event" id="event" required>
@@ -25,11 +55,10 @@
             </div>
             <label class="data" for="prerequis"><b>Prérequis (Optionnel, comme dans le cas d'un stage)</b></label>
             <input class="inputData form-control" type="text" placeholder="Prérequis" name="prerequis" id="prerequis">
-        </div>
-
-        <div class="col-12 col-md-8">
             <label class="data" for="description"><b>Description</b></label>
             <textarea class="inputData form-control" rows="10" cols="100" name="description" placeholder="Description.." id="description"></textarea>
+
+            <input class="hide" name="currentEvent" value="<?php echo $index?>" id="currentEvent">
         </div>
     </div>        
 
@@ -41,5 +70,26 @@
                 <button type="submit" class="btn-modObject annim" value="valide" name="submit" id="confirm">Valider</button>
             </div>
         </div>
-        
 </form> 
+
+<script src="scripts/previewPicture.js"></script>
+<script>
+if (<?php echo $index?> != -1){
+  if (<?php echo json_encode($currentEvent)?> != null){
+    document.getElementsByTagName("h1")[0].innerHTML = "Modifier les infos du Maître";
+    document.getElementById("confirm").value = "modify";
+    
+    //if we click on an existant master, we have to fill all the cointainer with data
+    document.getElementById("title").value = "<?php echo $currentEvent['title'] ?>";
+    let desc = "<?php echo $currentEvent['description'] ?>";
+    document.getElementById("description").value = desc.replaceAll('{n}', '\n');
+    document.getElementById("dateDebut").value = "<?php echo reformatDate($currentEvent['dateDebut'])?>";
+    document.getElementById("dateFin").value = "<?php echo reformatDate($currentEvent['dateFin'])?>";
+    document.getElementById("event").value = "<?php echo $currentEvent['type'] ?>";
+    document.getElementById("prerequis").value = "<?php echo $currentEvent['prerequis'] ?>";
+    document.getElementById("previewImgDiv").style = "background-image:url(<?php echo $currentEvent['image'] ?>);";
+    document.getElementById("oldImage").value = "<?php echo $currentEvent['image']?>"
+    document.getElementById("newImage").required = false;
+  }
+}
+</script>
