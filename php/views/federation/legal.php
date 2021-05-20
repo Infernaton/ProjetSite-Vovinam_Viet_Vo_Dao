83d51e9a5e4b92b86fd531fda651e14436503b9c
Infernaton <lubin.pauly@gmail.com>
2021-05-20 15:27:35 +0200

    mention légales dans le footer

--- php/views/federation/legal.php
+++ php/views/federation/legal.php
@@ -6,21 +6,6 @@
     </div>
 
         <h3>Chartre</h3>
-
-        <h3>Mentions Légales</h3>
-            <h5>Fédération VOVINAM-Viet Vo Dao France</h5>
-            <h5>Siège social : 6 allée des Agrions 91160, Ballainvilliers – France</h5>
-            <h5>Directeur de la publication : Amar GUERRIB</5>
-            <br>
-            <h5>Hébergeur: GANDI SAS</h5>
-            <h5>SAS au capital de 2 300 000 €</h5>
-            <h5>RCS Paris 423 093 459</h5>
-            <h5>Code APE 7740Z</h5>
-            <h5>N° TVA : FR 81423093459</h5>
-            <h5>Siège social : 63-65 boulevard de Masséna – France</h5>
-            <h5>Tél: 01.70.37.76.61</h5>
-
-
         
         <h3>RGPB</h3>
 
