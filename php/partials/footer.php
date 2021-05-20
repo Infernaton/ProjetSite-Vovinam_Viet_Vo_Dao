5843d7b645fb71d844da33a73f11ef383eaf7183
Infernaton <lubin.pauly@gmail.com>
2021-05-20 10:22:39 +0200

    Modification footer + suppression des pages html inutiles

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -3,21 +3,24 @@
 function partials_footer(){
 ?>   
     <footer class="mt-3">
-        <div class="fixed-bottom">
-            <div class="d-flex flex-row">
+        <div class="footer">
+            <div class="d-flex flex-row justify-content-around">
                 <div class="p-2 ">
-                    <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo-youtube2.png" id = "yt"></a>
+                    <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo-youtube2.png" width=25px></a>
                 </div>
                 <div class="p-2">
-                    <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/insta.png" id = "insta"></a>
+                    <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/insta.png" width=25px></a>
                 </div>
                 <div class="p-2">
-                    <a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank" ><img src="assets/img/fb.png"  id = "fb"></a>
+                    <a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank" ><img src="assets/img/fb.png"  width=25px></a>
                 </div>
                 <div class="p-2">
-                    <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/galerie.png"id = "galerie"></a>
+                    <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/galerie.png" width=25px></a>
                 </div>
             </div>
+        </div>
+        <div>
+        
         </div>
     </footer>
 </body>
