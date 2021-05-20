b6fb3a7deb49a4f7c1bd92fe3fdea12106d21c43
Infernaton <lubin.pauly@gmail.com>
2021-05-20 12:20:44 +0200

    design de footer (pas finis)

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -2,25 +2,33 @@
 
 function partials_footer(){
 ?>   
+<style>
+.list-group-item{
+    background-color: rgba(0,0,0,0);
+}
+</style>
     <footer class="">
         <div class="footer">
-            <div class="d-flex flex-row justify-content-around">
-                <div class="p-2 ">
-                    <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo-youtube2.png" width=25px></a>
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-2 col-md-6 col-lg-5">
+                    <ul class="list-group list-group-flush social-links">
+                    <li class="list-group-item">
+                        <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo-youtube2.png"><p class="will-hide">Chaîne Officielle de la Fédération</p></a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/fb.png"><p class="will-hide">Page Facebook Officielle</p></a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/insta.png"><p class="will-hide">Compte Instagram Officiel</p></a>
+                    </li>
+                    </ul>
                 </div>
-                <div class="p-2">
-                    <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/insta.png" width=25px></a>
-                </div>
-                <div class="p-2">
-                    <a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank" ><img src="assets/img/fb.png"  width=25px></a>
-                </div>
-                <div class="p-2">
-                    <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/galerie.png" width=25px></a>
+                <div class="col-6 col-md-1 col-lg-4"></div>
+                <div class="col-3 col-md-4 col-lg-2">
+                    <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/galerie.png" width=100px></a>
                 </div>
             </div>
-        </div>
-        <div>
-        
         </div>
     </footer>
 </body>
