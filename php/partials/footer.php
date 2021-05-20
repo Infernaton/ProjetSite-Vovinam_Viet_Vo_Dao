1aa6044412dedb83c23338a2521cccae86f8a28a
Infernaton <lubin.pauly@gmail.com>
2021-05-20 15:43:16 +0200

    Update Footer

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -8,18 +8,18 @@ function partials_footer(){
 }
 </style>
     <footer class="footer">
-        <div class="">
-            <div class="row ">
+        <div class="pt-3">
+            <div class="row">
                 <div class="col-2 col-md-7 col-lg-5 col-xl-3">
                     <ul class="list-group list-group-flush social-links">
                     <li class="list-group-item">
-                        <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo-youtube2.png"><p class="sm-hide">Chaîne Officielle de la Fédération</p></a>
+                        <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"><p class="sm-hide">Chaîne Officielle de la Fédération</p></a>
                     </li>
                     <li class="list-group-item">
-                        <a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/fb.png"><p class="sm-hide">Page Facebook Officielle</p></a>
+                        <a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/logo/fb.png"><p class="sm-hide">Page Facebook Officielle</p></a>
                     </li>
                     <li class="list-group-item">
-                        <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/insta.png"><p class="sm-hide">Compte Instagram Officiel</p></a>
+                        <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"><p class="sm-hide">Compte Instagram Officiel</p></a>
                     </li>
                     </ul>
                 </div>
@@ -31,14 +31,14 @@ function partials_footer(){
                 <div class="lg-hide col-xl-4 text-center">
                     <h5>Hébergeur: GANDI SAS</h5>
                     <p>SAS au capital de 2 300 000 €</p>
+                    <p><i class="fas fa-map-marker-alt"></i> 63-65 boulevard de Masséna – France</p>
                     <p>RCS Paris 423 093 459</p>
                     <p>Code APE 7740Z</p>
                     <p>N° TVA : FR 81423093459</p>
-                    <p><i class="fas fa-map-marker-alt"></i> 63-65 boulevard de Masséna – France</p>
                     <p>Tél: 01.70.37.76.61</p>
                 </div>
                 <div class="col-3 col-md-5 col-lg-3 col-xl-2 text-center">
-                    <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/galerie.png" width=150px></a>
+                    <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png" width=150px></a>
                 </div>
                 <div class="sm-hide col-md-12 only-lg-hide only-xl-hide text-center">
                     <hr>
