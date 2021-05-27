188fca79e704d1ca90a15f198f832153a0bda2b0
Infernaton <lubin.pauly@gmail.com>
2021-05-27 15:38:32 +0200

    Stylisation de texte avec petit racourcis

--- admin/views/modifyFAQ.php
+++ admin/views/modifyFAQ.php
@@ -1,8 +1,35 @@
 <?php
+    function str_replace_first($from, $to, $content){
+        $from = '/'.preg_quote($from, '/').'/';
+        return preg_replace($from, $to, $content, 1);
+    }
+
     $json_file = "../assets/json/faq.json";
     $faq = json_decode(file_get_contents($json_file), true, JSON_UNESCAPED_UNICODE);
     if ($_POST){
+        //Edit the response to make it easy to design in edit mode
+        $_POST['answer'] = str_replace("\r\n","<br>", $_POST['answer']);
+        $_POST['answer'] = str_replace("[", "<mark class='bg-danger'>", $_POST['answer']);
+        $_POST['answer'] = str_replace("]", "</mark>", $_POST['answer']);
+        for ($a=0; $a < substr_count($_POST['answer'], "**")/2; $a++) {
+            $_POST['answer'] = str_replace_first("**", "<b>", $_POST['answer']);
+            $_POST['answer'] = str_replace_first("**", "</b>", $_POST['answer']);
+        }
+        for ($a=0; $a < substr_count($_POST['answer'], "*")/2; $a++) {
+            $_POST['answer'] = str_replace_first("*", "<i>", $_POST['answer']);
+            $_POST['answer'] = str_replace_first("*", "</i>", $_POST['answer']);
+        }
+        for ($a=0; $a < substr_count($_POST['answer'], "_")/2; $a++) {
+            $_POST['answer'] = str_replace_first("_", "<ins>", $_POST['answer']);
+            $_POST['answer'] = str_replace_first("_", "</ins>", $_POST['answer']);
+        }
+        for ($a=0; $a < substr_count($_POST['answer'], "--")/2; $a++) {
+            $_POST['answer'] = str_replace_first("--", "<del>", $_POST['answer']);
+            $_POST['answer'] = str_replace_first("--", "</del>", $_POST['answer']);
+        }
         switch ($_POST['submit']){
+            case 'delete':
+                break;
             case 'valid':
                 $faq[$_POST['index']]['ask'] = $_POST['question'];
                 $faq[$_POST['index']]['rep'] = $_POST['answer'];
@@ -11,11 +38,10 @@
                 $new = '{"ask":"'.$_POST['question'].'","rep":"'.$_POST['answer'].'"}';
                 array_push($faq, json_decode($new, true, JSON_UNESCAPED_UNICODE));
                 break;
-            case 'delete':
-                break;
         }
         file_put_contents($json_file,json_encode($faq, JSON_PRETTY_PRINT));
     }
+    
 ?>
 <style>
     .QnA {
@@ -95,12 +121,7 @@
                         <h4><?php echo $faq[$i]['ask'];?></h4>
                     </div>
                     <div class="body">
-                        <p>
-                            <?php 
-                            $para = $faq[$i]['rep'];
-                            echo str_replace("\n","<br>", $para);
-                            ?>
-                        </p>
+                        <p><?php echo $faq[$i]['rep']."</mark></del></ins></i></b>";?></p>
                     </div>
                     <div class="footer text-right">
                         <button type="button" data-toggle="modal" data-target="#question-<?php echo $i?>">Modifier</button>
@@ -121,7 +142,17 @@
                             <h5>Question</h5>
                             <textarea class="inputData form-control" name="question" id="question"><?php echo $faq[$i]['ask'];?></textarea>
                             <h5>Réponse</h5>
-                            <textarea class="inputData form-control" name="answer" rows="10" id="answer"><?php echo $faq[$i]['rep'];?></textarea>
+                            <textarea class="inputData form-control" name="answer" rows="10" id="answer"><?php 
+                                    $para = $faq[$i]['rep'];
+                                    //Edit the response to make it easy to design in edit mode
+                                    $para = str_replace("<br>", "\r\n", $para);
+                                    $para = str_replace("<mark class='bg-danger'>", "[", $para);
+                                    $para = str_replace("</mark>", "]", $para);
+
+                                    $para = str_replace(["<b>","</b>", "<i>", "</i>", "<ins>", "</ins>", "<del>", "</del>"], ["**","**", "*", "*", "_", "_", "--", "--"], $para);
+                                    
+                                    echo $para;
+                                ?></textarea>
                         </div>
 
                         <div class="modal-footer">
