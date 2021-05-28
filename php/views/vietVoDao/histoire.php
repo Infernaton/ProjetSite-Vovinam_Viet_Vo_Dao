685ed8d4b02136e603f5a296adf4fcb76da610a0
Infernaton <lubin.pauly@gmail.com>
2021-05-28 10:27:37 +0200

    légère refonte graphique page histoire

--- php/views/vietVoDao/histoire.php
+++ php/views/vietVoDao/histoire.php
@@ -2,6 +2,16 @@
 $path = 'assets/img/le-vvd';
 $histoire = file_get_contents("assets/json/history.json");
 $histoire = json_decode($histoire, true, JSON_UNESCAPED_UNICODE);
+
+function printPara($paragraph){
+    global $path;
+    if ($paragraph[0] == '/'){
+        echo '<img src="'.$path.$paragraph.'"alt="" class="top-centered mt-5">';
+    }
+    else{
+        echo '<p class="mt-5">'.$paragraph.'</p>';
+    }
+}
 ?>
 <link rel="stylesheet" href="css/discipline.css">
 
@@ -28,32 +38,23 @@ body{
                     <div style="padding: 20px;">
                         <h2 class="content-title-yellow"><?php echo $partHistoire['title']?></h1>
                     </div>
-                    <div class="row">
-                        <div class="col-12 col-md-6">
+                    <div class="row p-2">
+                        <div class="md-hide col-lg-1"></div>
+                        <div class="col-12 col-md-6 col-lg-5">
                             <?php 
-                            //<img src="assets/img/le-vvd/groupe_fondateur.jpg" alt="" class="top-centered mt-5">
-                                foreach ($partHistoire['content1'] as $paragraph){
-                                    if ($paragraph[0] == '/'){
-                                        echo '<img src="'.$path.$paragraph.'"alt="" class="top-centered mt-5">';
-                                    }
-                                    else{
-                                        echo '<p class="mt-5">'.$paragraph.'</pa>';
-                                    }
-                                }
-                            ?>
-                        </div>
-                        <div class="col-12 col-md-6">
-                            <?php 
-                            foreach ($partHistoire['content2'] as $paragraph){
-                                if ($paragraph[0] == '/'){
-                                    echo '<img src="'.$path.$paragraph.'"alt="" class="top-centered mt-5">';
-                                }
-                                else{
-                                    echo '<p class="mt-5">'.$paragraph.'</p>';
-                                }
+                            foreach ($partHistoire['content1'] as $paragraph){
+                                printPara($paragraph);
                             }
                             ?>
                         </div>
+                        <div class="col-12 col-md-6 col-lg-5">
+                            <?php 
+                            foreach ($partHistoire['content2'] as $paragraph){
+                                printPara($paragraph);
+                            }
+                            ?>
+                        </div>
+                        <div class="md-hide col-lg-1"></div>
                     </div>
                 </div>
             </div>
@@ -64,29 +65,17 @@ body{
         <a id="sommaire" href="#" class="dropdown-toggle menu-init" data-toggle="dropdown" style="display: inline;"><i class="fas fa-bars"></i> Sommaire</a>
         <div id="sommaire">
             <div class="dropdown-menu">
+            <ul>
             <?php 
                 for ($i=0; $i<count($histoire);$i++){
-                    echo '<a href="#'.$histoire[$i]['id'].'" class="dropdown-item">'.$histoire[$i]['title'].'</a>';
+                    echo '<li>'.'<a href="#'.$histoire[$i]['id'].'" class="dropdown-item">'.$histoire[$i]['title'].'</a>'.'</li>';
                 }
             ?>
+            </ul>
             </div>
         </div>
         
     </div>
-<!--
-    <div class="container col-12 col-md-3 p-3 fixed-right" id='resume'>
-        <div class="text-center"><h3 class="content-title-blue">Sommaire</h3></div>
-        <hr>
-        <ul>
-            <a class="dropdown-item" href="?c=federation&p=0">La Fédération</a>
-
-        <?php 
-            for ($i=0; $i<count($histoire);$i++){
-                echo '<li style="margin-bottom:'.(100/count($histoire)).'%"><a href="#'.$histoire[$i]['id'].'" class="link">'.$histoire[$i]['title'].'</a></li>';
-            }
-        ?>
-        <ul>
-    </div>-->
 </div>
 <script>
 window.addEventListener("scroll", (event) => {
