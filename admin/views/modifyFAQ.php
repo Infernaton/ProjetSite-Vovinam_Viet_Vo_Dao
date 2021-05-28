2723d0737484191690a811431f635f254e23cf15
Infernaton <lubin.pauly@gmail.com>
2021-05-28 17:19:27 +0200

    doc écrite pour la stylisation du texte

--- admin/views/modifyFAQ.php
+++ admin/views/modifyFAQ.php
@@ -86,13 +86,72 @@
     <h1>Page FAQ</h1>
     <hr>
     <div>
-        <h3>Liste des questions</h3>
+        <h3>Liste des questions <i class="fas fa-question-circle" data-toggle="modal" data-target="#useSymbols"></i></h3>
+        <div class="modal fade" id="useSymbols">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3>Comment Styliser le texte des réponses ?</h3>
+                    </div>
+                    <div class="modal-body">
+                        <ul>
+                            <li>Souligner, mettre en gras/italique ... C'est possible de le faire ici pour le rendre plus vivant !</li>
+                        </ul>
+                        <p>Mais Comment ? C'est ce que nous allons voir maintenant.</p>
+                        <h4><ins>Styliser le texte</ins></h4>
+                        <p>
+                            Tout d'abord, il n'est possible de styliser uniquement la réponse, cela ne marche pas avec la question. <br>
+                            Il est ensuite possible de styliser le texte de 5 manières différentes:<br>
+                            <b>Gras</b>, <ins>Soulignement</ins>, <i>Italique</i>, <del>Barré</del>, et <mark class='bg-danger'>Mise en Valeur</mark>. <br> <br>
+                        </p>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-4">
+                                <p>
+                                    **Texte** → <b>Texte</b> <br>
+                                    <br>
+                                    _Texte_ → <ins>Texte</ins>
+                                </p>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4">
+                                <p>
+                                    *Texte* → <i>Texte</i> <br>
+                                    <br>
+                                    --Texte-- → <del>Texte</del>
+                                </p>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4">
+                                <p>
+                                    [Texte] => <mark class='bg-danger'>Texte</mark>
+                                </p>
+                            </div>
+                        </div>
+                        <p>
+                            On peux également fusionner des styles entre eux, par exemple faire un texte en italique et en gras: <br>
+                            ***Texte*** → <b><i>Texte</i></b> <br>
+                            Ou encore souligné et mettre en gras : <br>
+                            _**Texte**_ → <b><ins>Texte</ins></b> <br>
+                            Ou même appliquer du style à l'intérieur d'un autre style: <br>
+                            *Texte d'exemple, [texte] d'exemple* → <i>Texte d'exemple, <mark class='bg-danger'>texte</mark> d'exemple</i><br>
+                            <br>
+                            Il n'y a pas vraiment de limite au nombre de style que vous pouvez mettre dans une réponse, alors testez par vous même ! <br>
+                            <br>
+                            Pour finir, en mode Edition, le style choisit ne s'affichera pas, il ne sera montré qu'après avoir validé les nouveaux changements.
+                        </p>
+                        <h4><ins>Liens Hypertextes</ins></h4>
+                        <p>
+                            Pour créer des liens cliquables dans un réponse, rien de plus simple, vous avez juste à mettre votre liens dans votre réponse. Et comme pour la stylisation, il sera transformé en liens cliquable apres validation des changements. <br>
+                            https://www.exemple_de_liens.fr => <a href="">https://www.exemple_de_liens.fr</a> <br> (<i>Lien d'exemple, ne fonctionne pas</i>)
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="text-center">
             <button type="button" data-toggle="modal" data-target="#add-question">
                 <h4>+ Ajouter une nouvelle question</h3>
             </button>
         </div>
-        <div class="modal" id="add-question">
+        <div class="modal fade" id="add-question">
                 <div class="modal-dialog modal-lg">
                     <form action="" method="post" enctype="multipart/form-data">
                     <div class="modal-content">
@@ -140,7 +199,7 @@
                     <button type="button" data-toggle="modal" data-target="#question-<?php echo $i?>">Modifier</button>
                 </div>
             </div>
-            <div class="modal" id="question-<?php echo $i?>">
+            <div class="modal fade" id="question-<?php echo $i?>">
                 <div class="modal-dialog modal-lg">
                     <form action="" method="post" enctype="multipart/form-data">
 
