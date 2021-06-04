be4c523c48541cc7ba7cd55c987c33a6557262f1
Mathis <mathisouchikh@gmail.com>
2021-06-04 15:53:09 +0200

    Update footer wip pour la réunion

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -10,8 +10,9 @@ function partials_footer(){
 </style>
     <footer class="footer">
         <div class="pt-3">
-            <div class="row"> 
-                <div class="d-flex flex-row">
+            
+                
+                <div class="d-flex justify-content-start">
                         <ul class="list-group list-group-flush social-links">
                         <li class="list-group-item p-2">
                             <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"><p>Chaîne Officielle de la Fédération</p></a>
@@ -23,10 +24,11 @@ function partials_footer(){
                             <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"><p>Compte Instagram Officiel</p></a>
                         </li>
                         </ul>
-                        <div class="sm-hide col-md-5 col-lg-3 col-xl-2 text-center">
+                    
+                        <li class="list-group-item p-2">
                             <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png" width=150px></a>
-                        </div>
-                    </div>
+                            </li>
+                    
                     <div class="row">  
                         <div class="d-flex flex-row">  
                             <div class="col-4 only-md-hide only-lg-hide only-xl-hide">
@@ -46,14 +48,15 @@ function partials_footer(){
                         </div>
                     </div>
                 </div>
-            </div>    
+              
             <div class="pt-3">
+            <div class="d-flex justify-content-center">
                         <!--<h5>Fédération VOVINAM-Viet Vo Dao France</h5>-->
-                        <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France</p>
+                        <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France |  </p>
                         
-                        <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com</p>
-                        <p><i class="fas fa-phone"></i> 01.64.49.60.10</p>
-                        <p><a href="?c=federation&p=4">Mentions Légales</a></p>
+                        <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com |  </p>
+                        <p><i class="fas fa-phone"></i> 01.64.49.60.10 |  </p>
+                        <p><a href="?c=federation&p=4"> Mentions Légales |  </a></p>
                     </div>
                 </div>
                 <!--<div class="sm-hide col-md-5 col-lg-3 col-xl-2 text-center">
@@ -63,13 +66,14 @@ function partials_footer(){
                             <hr>
                             <h4>Mentions Légales</h4>
                             <h5>Fédération VOVINAM-Viet Vo Dao France</h5>
-                            <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France</p>
-                            <p><i class="fas fa-user-alt"></i> Directeur de la publication : Amar GUERRIB</p>
-                            <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com</p>
+                            <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France | </p>
+                            <p><i class="fas fa-user-alt"></i> Directeur de la publication : Amar GUERRIB | </p>
+                            <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com | </p>
                             <p><i class="fas fa-phone"></i> 01.64.49.60.10</p>
                         </div>
                     </div>
                 </div>
+            </div>
             </div>
         </div>
     </footer>
