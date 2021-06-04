affaeb7cf1a21548937b9c0716b1a14f9cc12ef1
Mathis <mathisouchikh@gmail.com>
2021-06-04 15:08:18 +0200

    Nouveau Footer WiP

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -10,55 +10,64 @@ function partials_footer(){
 </style>
     <footer class="footer">
         <div class="pt-3">
-            <div class="row">
-                <div class="sm-hide col-md-7 col-lg-5 col-xl-3">
-                    <ul class="list-group list-group-flush social-links">
-                    <li class="list-group-item">
-                        <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"><p>Chaîne Officielle de la Fédération</p></a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/logo/fb.png"><p>Page Facebook Officielle</p></a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"><p>Compte Instagram Officiel</p></a>
-                    </li>
-                    </ul>
-                </div>
-                <div class="col-4 only-md-hide only-lg-hide only-xl-hide">
-                    <div class="list-group list-group-flush social-links">
-                        <div class="list-group-item"><a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"><p>Youtube</p></a></div>
-                        <div class="list-group-item"><a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/logo/fb.png"><p>Facebook</p></a></div>
-                        <div class="list-group-item"><a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"><p>Instagram</p></a></div>
-                        <div class="list-group-item"><a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png"><p>Galerie Photo</p></a></div>
+            <div class="row"> 
+                <div class="d-flex flex-row">
+                        <ul class="list-group list-group-flush social-links">
+                        <li class="list-group-item p-2">
+                            <a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"><p>Chaîne Officielle de la Fédération</p></a>
+                        </li>
+                        <li class="list-group-item p-2">
+                            <a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/logo/fb.png"><p>Page Facebook Officielle</p></a>
+                        </li>
+                        <li class="list-group-item p-2">
+                            <a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"><p>Compte Instagram Officiel</p></a>
+                        </li>
+                        </ul>
+                        <div class="sm-hide col-md-5 col-lg-3 col-xl-2 text-center">
+                            <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png" width=150px></a>
+                        </div>
+                    </div>
+                    <div class="row">  
+                        <div class="d-flex flex-row">  
+                            <div class="col-4 only-md-hide only-lg-hide only-xl-hide">
+                                <div class="list-group list-group-flush social-links">
+                                    <div class="list-group-item"><a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"><p>Youtube</p></a></div>
+                                    <div class="list-group-item"><a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/logo/fb.png"><p>Facebook</p></a></div>
+                                    <div class="list-group-item"><a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"><p>Instagram</p></a></div>
+                                    <div class="list-group-item"><a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png"><p>Galerie Photo</p></a></div>
+                                </div>
+                            </div>
+                            <div class="lg-hide col-xl-2 d-flex justify-content-center">
+                                <div class="p-2 center">
+                                <!--<a href="?c=federation&p=4"><h4>Mentions Légales</h4></a>-->
+                                    
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="lg-hide col-xl-2 d-flex justify-content-center">
-                    <div class="p-2 center">
-                    <a href="?c=federation&p=4"><h4>Mentions Légales</h4></a>
-                        
-                    </div>
-                </div>
-                <div class="col-8 col-sm-8 only-md-hide col-lg-4 col-xl-5 d-flex justify-content-center">
-                    <div class="p-2">
-                        <h5>Fédération VOVINAM-Viet Vo Dao France</h5>
+            </div>    
+            <div class="pt-3">
+                        <!--<h5>Fédération VOVINAM-Viet Vo Dao France</h5>-->
                         <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France</p>
                         
                         <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com</p>
                         <p><i class="fas fa-phone"></i> 01.64.49.60.10</p>
+                        <p><a href="?c=federation&p=4">Mentions Légales</a></p>
                     </div>
                 </div>
-                <div class="sm-hide col-md-5 col-lg-3 col-xl-2 text-center">
+                <!--<div class="sm-hide col-md-5 col-lg-3 col-xl-2 text-center">
                     <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png" width=150px></a>
-                </div>
-                <div class="sm-hide col-md-12 only-lg-hide only-xl-hide d-flex justify-content-center">
-                    <div class="p-2">
-                        <hr>
-                        <h4>Mentions Légales</h4>
-                        <h5>Fédération VOVINAM-Viet Vo Dao France</h5>
-                        <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France</p>
-                        <p><i class="fas fa-user-alt"></i> Directeur de la publication : Amar GUERRIB</p>
-                        <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com</p>
-                        <p><i class="fas fa-phone"></i> 01.64.49.60.10</p>
+                </div>-->
+                    <div class="sm-hide col-md-12 only-lg-hide only-xl-hide d-flex justify-content-center">
+                            <hr>
+                            <h4>Mentions Légales</h4>
+                            <h5>Fédération VOVINAM-Viet Vo Dao France</h5>
+                            <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France</p>
+                            <p><i class="fas fa-user-alt"></i> Directeur de la publication : Amar GUERRIB</p>
+                            <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com</p>
+                            <p><i class="fas fa-phone"></i> 01.64.49.60.10</p>
+                        </div>
                     </div>
                 </div>
             </div>
