7e304235c767d24fd151e130a3b38b5c01e66bcd
Infernaton <lubin.pauly@gmail.com>
2021-06-08 15:12:26 +0200

    Début de la page actualité WIP

---
+++ php/views/actualite.php
@@ -0,0 +1,41 @@
+<style>
+#category .list-group-item-action{
+    transition: auto;
+}
+</style>
+<div class="container">
+    <div class="text-center mb-5">
+        <h1 class="content-title-blue">Actualités</h1>
+    </div>
+    <div class="row">
+        <div class="col-10">
+            <div id="lastInfo">
+                <h3>Dernière Actualité</h3>
+                <div class="content-main">
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, <br>
+                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt <br>
+                explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni <br>
+                dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, <br>
+                consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam <br>
+                quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, <br>
+                nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate <br>
+                velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"<br>
+                </div>
+            </div>
+            <div class="" id="thread">
+                <h3>Précédentes Actualités</h3>
+            </div>
+        </div>
+        
+        <div class="col-2" id="category">
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-dark">Catégorie</li>
+                <a href="#" class="list-group-item list-group-item-action">First item</a>
+                <a href="#" class="list-group-item list-group-item-action">First item</a>
+                <a href="#" class="list-group-item list-group-item-action">First item</a>
+                <a href="#" class="list-group-item list-group-item-action">First item</a>
+                <a href="#" class="list-group-item list-group-item-action">First item</a>
+            </ul>
+        </div>
+    </div>
+</div>
