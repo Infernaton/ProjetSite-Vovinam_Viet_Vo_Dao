ad8ba21ad5fc9689d37395e44d3bd9fed66bbd01
Infernaton <lubin.pauly@gmail.com>
2021-06-09 19:16:06 +0200

    footer simplist

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -9,68 +9,31 @@ footer .list-group-item{
 }
 </style>
     <footer class="footer">
-        <div class="pt-3">
-            
-                
-                <div class="d-flex flex-row bd-highlight mb-3">
-                
-                        <ul class="list-group list-group-flush social-links">
-                        <div class="link">
-                        
-                            <a class="p-2 bd-highlight" href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"><p>Chaîne Officielle de la Fédération</p></a>
-                        
-                            <a class="p-2 bd-highlight" href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/logo/fb.png"><p>Page Facebook Officielle</p></a>
-                        
-                           <a class="p-2 bd-highlight" href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"><p>Compte Instagram Officiel</p></a>
-
-                        </div>
-                        </ul>
-
-                            <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png" width=150px></a>
-                        
-                        
-                    
-                    <div class="row">  
-                        <div class="d-flex flex-row">  
-                            <div class="col-4 only-md-hide only-lg-hide only-xl-hide">
-                                <div class="list-group list-group-flush social-links">
-                                    <div class="list-group-item"><a href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"><p>Youtube</p></a></div>
-                                    <div class="list-group-item"><a href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/logo/fb.png"><p>Facebook</p></a></div>
-                                    <div class="list-group-item"><a href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"><p>Instagram</p></a></div>
-                                    <div class="list-group-item"><a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png"><p>Galerie Photo</p></a></div>
-                                </div>
-                            </div>               
-                                
-                            
-                        </div>
-                    </div>
-                </div>
-                
-            <div class="pt-3">
+        <div class="pt-3 address" style="background-color: #1a498a;">
             <div class="d-flex justify-content-center">
-                        <!--<h5>Fédération VOVINAM-Viet Vo Dao France</h5>-->
-                        <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France |  
-                        <i class="fas fa-envelope"></i> vovinamfr@vietvodao.com |  
-                        <i class="fas fa-phone"></i> 01.64.49.60.10 |  
-                        <a href="?c=federation&p=4"> Mentions Légales |  </a></p>
-                    </div>
-                </div>
-                <!--<div class="sm-hide col-md-5 col-lg-3 col-xl-2 text-center">
-                    <a href="http://vietvodao.photos/" target="_blank" ><img src="assets/img/logo/galerie.png" width=150px></a>
-                </div>-->
-                    <div class="sm-hide col-md-12 only-lg-hide only-xl-hide d-flex justify-content-center">
-                            <hr>
-                            <h4>Mentions Légales</h4>
-                            <h5>Fédération VOVINAM-Viet Vo Dao France</h5>
-                            <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France | </p>
-                            <p><i class="fas fa-user-alt"></i> Directeur de la publication : Amar GUERRIB | </p>
-                            <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com | </p>
-                            <p><i class="fas fa-phone"></i> 01.64.49.60.10</p>
-                        </div>
-                    </div>
-                </div>
+                <!--<h5>Fédération VOVINAM-Viet Vo Dao France</h5>-->
+                <p>
+                    <i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France |
+                    <i class="fas fa-envelope"></i> vovinamfr@vietvodao.com |
+                    <i class="fas fa-phone"></i> 01.64.49.60.10 |
+                    <a href="?c=federation&p=4">Mentions Légales</a>
+                </p>
             </div>
+            <div class="social-links d-flex justify-content-center mt-2">
+                <a class="" href="https://www.instagram.com/federationvovinamvietvodaofr/" target="_blank" ><img src="assets/img/logo/insta.png"></a>
+                <a class="" href="https://www.facebook.com/FederationVovinamVietVoDaoFrance" target="_blank"><img src="assets/img/logo/fb.png"></a>
+                <a class="" href="https://www.youtube.com/channel/UC87j8G2WRUQur4VCHjF3OKQ" target="_blank" ><img src="assets/img/logo/yt.png"></a>
             </div>
+            <hr>
+        </div>
+        <div class="sm-hide col-md-12 only-lg-hide only-xl-hide d-flex justify-content-center">
+            <hr>
+            <h4>Mentions Légales</h4>
+            <h5>Fédération VOVINAM-Viet Vo Dao France</h5>
+            <p><i class="fas fa-map-marker-alt"></i> 6 allée des Agrions 91160, Ballainvilliers – France | </p>
+            <p><i class="fas fa-user-alt"></i> Directeur de la publication : Amar GUERRIB | </p>
+            <p><i class="fas fa-envelope"></i> vovinamfr@vietvodao.com | </p>
+            <p><i class="fas fa-phone"></i> 01.64.49.60.10</p>
         </div>
     </footer>
 </body>
