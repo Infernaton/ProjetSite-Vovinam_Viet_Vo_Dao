7be1c500702ae403972cf7fe0c5d666f4a33749d
Mathis <mathisouchikh@gmail.com>
2021-06-09 16:03:47 +0200

    Update contacts.php

--- php/views/federation/contacts.php
+++ php/views/federation/contacts.php
@@ -1,5 +1,17 @@
 <link rel="stylesheet" href="css/federation.css">
-
+<style>
+h5{
+        display: inline-block;
+        vertical-align:baseline; 
+}
+h4{
+        display: inline-block;
+        vertical-align:baseline; 
+}
+.bureau{
+        margin-right: 300px;
+}
+</style>
 <div class="container info">
         <div class="text-center pb-3">
                 <h1 class="content-title-yellow"> Contacts </h1>
@@ -9,13 +21,15 @@
        
        <p>
         <h4>Bureau: </h4>
-        <strong>  Président Fédéral:</strong> Me Mai GUERRIB - presidentfr@vietvodao.com
-        <br>
-        <strong>  Secrétariat Général:</strong> Me Rudy HANAFI - secretairegeneral@vietvodao.com
-        <br>
-        <strong>  Trésorier:</strong>  Franck MALBOT - tresorierfr@vietvodao.com
-        <br>
-        <strong>Gestionnaire de licence fédérale:</strong> Me Rudy HANAFI - Secretariat@vietvodao.com - 64, rue du Parc d’Athis 91200 Athis-Mons
+        <div class="bureau">
+                <h5>  Président Fédéral:</h5> Me Mai GUERRIB -presidentfr@vietvodao.com
+                <br><br>
+                <h5>  Secrétariat Général:</h5> Me Rudy HANAFI - secretairegeneral@vietvodao.com
+                <br><br>
+                <h5>  Trésorier:</h5>  Franck MALBOT - tresorierfr@vietvodao.com
+                <br><br>
+        </div>
+        <h4 class="title">Gestionnaire de licence fédérale:</h4> Me Rudy HANAFI <br> Secretariat@vietvodao.com <br> 64, rue du Parc d’Athis 91200 Athis-Mons
         
        </p>
 </div>
