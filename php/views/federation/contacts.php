29329c3536493b53a85aa7bf605d01821d940c50
Mathis <mathisouchikh@gmail.com>
2021-06-10 16:29:28 +0200

    Update organigramme

--- php/views/federation/contacts.php
+++ php/views/federation/contacts.php
@@ -20,7 +20,6 @@ h4{
        <p>
         
         <h4>Bureau: </h4>
-        <br>
         <div class="sub">
                 <h5>  Président Fédéral:</h5> Me. Mai GUERRIB -presidentfr@vietvodao.com
 
@@ -37,29 +36,34 @@ h4{
                 <br>
                 <br>
         </div>
-        <h4 class="title">Gestionnaire de licence fédérale:</h4> Me. Rudy HANAFI <br> Secretariat@vietvodao.com <br> 64, rue du Parc d’Athis 91200 Athis-Mons
-
+        <h4 class="title">Gestionnaire de licence fédérale:</h4> <br> 
+                <div class="sub">
+                Me. Rudy HANAFI - Secretariat@vietvodao.com - 64, rue du Parc d’Athis 91200 Athis-Mons
+                </div>
         <br>
         <br>
 
-        <h4 class="title">Le Président du Conseil des Maitres:</h4> Me. MIESCH Philippe <br> Conseildesmaitres@vietvodao.com  <br> philippe.miesch@gmail.com 
-
+        <h4 class="title">Le Président du Conseil des Maitres:</h4>
+                <div class="sub">
+                Me. MIESCH Philippe <br> Conseildesmaitres@vietvodao.com  <br> philippe.miesch@gmail.com 
+                </div>
         <br>
         <br>
 
-        <h4 class="title">Le Directeur Technique National:</h4> Me. GUERRIB Amar <br> dtn@vietvodao.com  <br> aguerrib@yahoo.fr
-
-
+        <h4 class="title">Le Directeur Technique National:</h4>
+                <div class="sub">
+                Me. GUERRIB Amar <br> dtn@vietvodao.com  <br> aguerrib@yahoo.fr
+                </div>
+        <br>
        </p>
 
        <h2>Comités Régionaux </h2>
         <br>
        <p>
         
-        <h4> Ile de France:</h4>
-        <br>
+        <h4 class="title"> Ile de France:</h4>
         <div class="sub">
-         (<a href="www.vovinam-idf.fr">www.vovinam-idf.fr</a>)
+         <a href="www.vovinam-idf.fr">www.vovinam-idf.fr</a>
          <br>
                 <h5>Président du Comité:</h5> M. LE CHEVALLIER Gilles - pdt.idf@vietvodao.com
                 <br>
@@ -71,10 +75,9 @@ h4{
         <br>
 
         
-        <h4> Nouvelle-Aquitaine:</h4>
-        <br>
+        <h4 class="title"> Nouvelle-Aquitaine:</h4>
         <div class="sub">
-         (<a href="www.vovinam-limousin.fr">www.vovinam-limousin.fr</a>/<a href="www.vovinampoitiers.fr">www.vovinampoitiers.fr</a>)
+         <a href="www.vovinam-limousin.fr">www.vovinam-limousin.fr</a>/<a href="www.vovinampoitiers.fr">www.vovinampoitiers.fr</a>
          <br>
                 <h5>Présidente du Comité:</h5> Mme. HAMMANN Magalie - pdt.nouvelleaquitaine@vietvodao.com
                 <br>
@@ -86,10 +89,9 @@ h4{
         <br>
 
         
-        <h4> Auvergne-Rhône-Alpes:</h4>
-        <br>
+        <h4 class="title"> Auvergne-Rhône-Alpes:</h4>
         <div class="sub">
-         (<a href="www.vietvodaolyon.free.fr">www.vietvodaolyon.free.fr</a>)
+         <a href="www.vietvodaolyon.free.fr">www.vietvodaolyon.free.fr</a>
          <br>
                 <h5>Présidente du Comité:</h5> Mme. THONGDARA Stéphanie - pdt.rhonealpes@vietvodao.com
                 <br>
@@ -101,10 +103,9 @@ h4{
         <br>
 
         
-        <h4> Normandie:</h4>
-        <br>
+        <h4 class="title"> Normandie:</h4>
         <div class="sub">
-         (<a href="www.vovinam-caen.com">www.vovinam-caen.com</a>)
+         <a href="www.vovinam-caen.com">www.vovinam-caen.com</a>
          <br>
                 <h5>Président du Comité:</h5> M. DEBROU Bruno - pdt.bassenormandie@vietvodao.com
                 <br>
@@ -116,10 +117,9 @@ h4{
         <br>
 
         
-        <h4>  Grand-Est:</h4>
-        <br>
+        <h4 class="title">  Grand-Est:</h4>
         <div class="sub">
-         (<a href=" www.vovinam-reims.com"> www.vovinam-reims.com</a>)
+         <a href="www.vovinam-reims.com"> www.vovinam-reims.com</a>
          <br>
                 <h5>Président du Comité:</h5> M. Cédric Piasecki - pdt.champagne@vietvodao.com
                 <br>
@@ -131,15 +131,15 @@ h4{
         <br>
 
         
-        <h4>  Bretagne:</h4>
-        <br>
+        <h4 class="title">  Bretagne:</h4>
         <div class="sub">
-         (<a href=" www.vovinam-rennes.fr"> www.vovinam-rennes.fr</a>)
+         <a href="www.vovinam-rennes.fr"> www.vovinam-rennes.fr</a>
          <br>
                 <h5>Président du Comité:</h5> M. Rolland RACOTEAU - pdt.bretagne@vietvodao.com
                 <br>
 
                 <h5>Responsable Technique: </h5> Maitre. Emmanuel GREGORI - rt.bretagne@vietvodao.com
         </div>
+        <br>
        </p>
 </div>
