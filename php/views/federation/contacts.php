ff91d4500b84e4f5a3d2f680c92768addff1019c
Mathis <mathisouchikh@gmail.com>
2021-06-11 13:03:53 +0200

    Ajout d'une bordure à l'organigramme

--- php/views/federation/contacts.php
+++ php/views/federation/contacts.php
@@ -18,128 +18,130 @@ h4{
         <h2>Organigramme</h2>
         <br>
        <p>
-        
-        <h4>Bureau: </h4>
-        <div class="sub">
-                <h5>  Président Fédéral:</h5> Me. Mai GUERRIB -presidentfr@vietvodao.com
+        <div class="border">
+                <h4>Bureau: </h4>
+                <div class="sub">
+                        <h5>  Président Fédéral:</h5> Me. Mai GUERRIB -presidentfr@vietvodao.com
 
+                        <br>
+                        <br>
+
+                        <h5>  Secrétariat Général:</h5> Me. Rudy HANAFI - secretairegeneral@vietvodao.com
+
+                        <br>
+                        <br>
+
+                        <h5>  Trésorier:</h5>  Franck MALBOT - tresorierfr@vietvodao.com
+
+                        <br>
+                        <br>
+                </div>
+                <h4 class="title">Gestionnaire de licence fédérale:</h4> <br> 
+                        <div class="sub">
+                        Me. Rudy HANAFI - Secretariat@vietvodao.com - 64, rue du Parc d’Athis 91200 Athis-Mons
+                        </div>
                 <br>
                 <br>
 
-                <h5>  Secrétariat Général:</h5> Me. Rudy HANAFI - secretairegeneral@vietvodao.com
-
+                <h4 class="title">Le Président du Conseil des Maitres:</h4>
+                        <div class="sub">
+                        Me. MIESCH Philippe <br> Conseildesmaitres@vietvodao.com  <br> philippe.miesch@gmail.com 
+                        </div>
                 <br>
                 <br>
 
-                <h5>  Trésorier:</h5>  Franck MALBOT - tresorierfr@vietvodao.com
-
-                <br>
+                <h4 class="title">Le Directeur Technique National:</h4>
+                        <div class="sub">
+                        Me. GUERRIB Amar <br> dtn@vietvodao.com  <br> aguerrib@yahoo.fr
+                        </div>
                 <br>
         </div>
-        <h4 class="title">Gestionnaire de licence fédérale:</h4> <br> 
-                <div class="sub">
-                Me. Rudy HANAFI - Secretariat@vietvodao.com - 64, rue du Parc d’Athis 91200 Athis-Mons
-                </div>
-        <br>
-        <br>
-
-        <h4 class="title">Le Président du Conseil des Maitres:</h4>
-                <div class="sub">
-                Me. MIESCH Philippe <br> Conseildesmaitres@vietvodao.com  <br> philippe.miesch@gmail.com 
-                </div>
-        <br>
-        <br>
-
-        <h4 class="title">Le Directeur Technique National:</h4>
-                <div class="sub">
-                Me. GUERRIB Amar <br> dtn@vietvodao.com  <br> aguerrib@yahoo.fr
-                </div>
-        <br>
        </p>
 
        <h2>Comités Régionaux </h2>
         <br>
        <p>
-        
-        <h4 class="title"> Ile de France:</h4>
-        <div class="sub">
-         <a href="www.vovinam-idf.fr">www.vovinam-idf.fr</a>
-         <br>
-                <h5>Président du Comité:</h5> M. LE CHEVALLIER Gilles - pdt.idf@vietvodao.com
+        <div class="border">
+                <h4 class="title"> Ile de France:</h4>
+                <div class="sub">
+                <a href="www.vovinam-idf.fr">www.vovinam-idf.fr</a>
+                <br>
+                        <h5>Président du Comité:</h5> M. LE CHEVALLIER Gilles - pdt.idf@vietvodao.com
+                        <br>
+
+                        <h5>Responsable Technique: </h5> Maitre. LAMA David - rt.idf@vietvodao.com
+                </div>
+
+                <br>
                 <br>
 
-                <h5>Responsable Technique: </h5> Maitre. LAMA David - rt.idf@vietvodao.com
-        </div>
+                
+                <h4 class="title"> Nouvelle-Aquitaine:</h4>
+                <div class="sub">
+                <a href="www.vovinam-limousin.fr">www.vovinam-limousin.fr</a>/<a href="www.vovinampoitiers.fr">www.vovinampoitiers.fr</a>
+                <br>
+                        <h5>Présidente du Comité:</h5> Mme. HAMMANN Magalie - pdt.nouvelleaquitaine@vietvodao.com
+                        <br>
 
-        <br>
-        <br>
+                        <h5>Responsable Technique: </h5> Maitre. TA Jean-Pierre - rt.nouvelleaquitaine@vietvodao.com
+                </div>
 
-        
-        <h4 class="title"> Nouvelle-Aquitaine:</h4>
-        <div class="sub">
-         <a href="www.vovinam-limousin.fr">www.vovinam-limousin.fr</a>/<a href="www.vovinampoitiers.fr">www.vovinampoitiers.fr</a>
-         <br>
-                <h5>Présidente du Comité:</h5> Mme. HAMMANN Magalie - pdt.nouvelleaquitaine@vietvodao.com
+                <br>
                 <br>
 
-                <h5>Responsable Technique: </h5> Maitre. TA Jean-Pierre - rt.nouvelleaquitaine@vietvodao.com
-        </div>
+                
+                <h4 class="title"> Auvergne-Rhône-Alpes:</h4>
+                <div class="sub">
+                <a href="www.vietvodaolyon.free.fr">www.vietvodaolyon.free.fr</a>
+                <br>
+                        <h5>Présidente du Comité:</h5> Mme. THONGDARA Stéphanie - pdt.rhonealpes@vietvodao.com
+                        <br>
 
-        <br>
-        <br>
+                        <h5>Responsable Technique: </h5> Maître. THONGDARA Frédéric - rt.rhonealpes@vietvodao.com
+                </div>
 
-        
-        <h4 class="title"> Auvergne-Rhône-Alpes:</h4>
-        <div class="sub">
-         <a href="www.vietvodaolyon.free.fr">www.vietvodaolyon.free.fr</a>
-         <br>
-                <h5>Présidente du Comité:</h5> Mme. THONGDARA Stéphanie - pdt.rhonealpes@vietvodao.com
+                <br>
                 <br>
 
-                <h5>Responsable Technique: </h5> Maître. THONGDARA Frédéric - rt.rhonealpes@vietvodao.com
-        </div>
+                
+                <h4 class="title"> Normandie:</h4>
+                <div class="sub">
+                <a href="www.vovinam-caen.com">www.vovinam-caen.com</a>
+                <br>
+                        <h5>Président du Comité:</h5> M. DEBROU Bruno - pdt.bassenormandie@vietvodao.com
+                        <br>
 
-        <br>
-        <br>
-
-        
-        <h4 class="title"> Normandie:</h4>
-        <div class="sub">
-         <a href="www.vovinam-caen.com">www.vovinam-caen.com</a>
-         <br>
-                <h5>Président du Comité:</h5> M. DEBROU Bruno - pdt.bassenormandie@vietvodao.com
+                        <h5>Responsable Technique: </h5> Maitre. Emmanuel GREGORI - rt.bassenormandie@vietvodao.com
+                </div>
+                
+                <br>
                 <br>
 
-                <h5>Responsable Technique: </h5> Maitre. Emmanuel GREGORI - rt.bassenormandie@vietvodao.com
-        </div>
-        
-        <br>
-        <br>
+                
+                <h4 class="title">  Grand-Est:</h4>
+                <div class="sub">
+                <a href="www.vovinam-reims.com"> www.vovinam-reims.com</a>
+                <br>
+                        <h5>Président du Comité:</h5> M. Cédric Piasecki - pdt.champagne@vietvodao.com
+                        <br>
 
-        
-        <h4 class="title">  Grand-Est:</h4>
-        <div class="sub">
-         <a href="www.vovinam-reims.com"> www.vovinam-reims.com</a>
-         <br>
-                <h5>Président du Comité:</h5> M. Cédric Piasecki - pdt.champagne@vietvodao.com
+                        <h5>Responsable Technique: </h5> Maître. NGUYEN Dinh Hoang - rt.champagne@vietvodao.com
+                </div>
+                
+                <br>
                 <br>
 
-                <h5>Responsable Technique: </h5> Maître. NGUYEN Dinh Hoang - rt.champagne@vietvodao.com
-        </div>
-        
-        <br>
-        <br>
-
-        
-        <h4 class="title">  Bretagne:</h4>
-        <div class="sub">
-         <a href="www.vovinam-rennes.fr"> www.vovinam-rennes.fr</a>
-         <br>
-                <h5>Président du Comité:</h5> M. Rolland RACOTEAU - pdt.bretagne@vietvodao.com
+                
+                <h4 class="title">  Bretagne:</h4>
+                <div class="sub">
+                <a href="www.vovinam-rennes.fr"> www.vovinam-rennes.fr</a>
                 <br>
+                        <h5>Président du Comité:</h5> M. Rolland RACOTEAU - pdt.bretagne@vietvodao.com
+                        <br>
 
-                <h5>Responsable Technique: </h5> Maitre. Emmanuel GREGORI - rt.bretagne@vietvodao.com
-        </div>
-        <br>
+                        <h5>Responsable Technique: </h5> Maitre. Emmanuel GREGORI - rt.bretagne@vietvodao.com
+                </div>
+                <br>
+        </div>        
        </p>
 </div>
