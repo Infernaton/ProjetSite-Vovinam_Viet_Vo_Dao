2a9dc08896951a02f4631bee6ca4b37ca6da1b1a
Infernaton <lubin.pauly@gmail.com>
2021-06-16 18:00:22 +0200

    Actualité v1

--- php/views/actualite.php
+++ php/views/actualite.php
@@ -1,3 +1,26 @@
+<?php
+$req = $db->query('SELECT * FROM news ORDER BY date');
+$news = $req->fetchAll(PDO::FETCH_ASSOC);
+
+function printLastNews($news){
+?>
+    <div id="lastInfo">
+        <h3>Dernière Actualité</h3>
+        <div class="content-main border">
+            <div class="row">
+                <div class="col-10">
+                    <h5><?php echo $news['title']?></h5>
+                    <p><?php echo $news['content']?></p>
+                </div>
+                <div class="col-2">
+                    <img class="img_fill" src="assets/img/no-picture.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+}
+?>
 <style>
 #category .list-group-item-action{
     transition: auto;
@@ -7,26 +30,16 @@
     <div class="text-center mb-5">
         <h1 class="content-title-blue">Actualités</h1>
     </div>
+    <?php printLastNews($news[0]); ?>
+    <div class="" id="thread">
+        <h3>Précédentes Actualités</h3>
+    </div>
     <div class="row">
-        <div class="col-10">
-            <div id="lastInfo">
-                <h3>Dernière Actualité</h3>
-                <div class="content-main">
-                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, <br>
-                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt <br>
-                explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni <br>
-                dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, <br>
-                consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam <br>
-                quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, <br>
-                nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate <br>
-                velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"<br>
-                </div>
-            </div>
-            <div class="" id="thread">
-                <h3>Précédentes Actualités</h3>
-            </div>
+        <div class="col-4">
+
         </div>
-        
+    </div>
+    <!--
         <div class="col-2" id="category">
             <ul class="list-group">
                 <li class="list-group-item list-group-item-dark">Catégorie</li>
@@ -36,6 +49,5 @@
                 <a href="#" class="list-group-item list-group-item-action">First item</a>
                 <a href="#" class="list-group-item list-group-item-action">First item</a>
             </ul>
-        </div>
-    </div>
+        </div>-->
 </div>
