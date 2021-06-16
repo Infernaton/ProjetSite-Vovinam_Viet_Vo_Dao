0ba095b5905459cc8e57d99c28a03b8acd547564
Mathis <mathisouchikh@gmail.com>
2021-06-16 15:19:08 +0200

    Update lien.php

--- php/views/federation/lien.php
+++ php/views/federation/lien.php
@@ -8,35 +8,38 @@
             <h1 class="content-title-yellow"> Liens </h1>
         </div>
         <!-- Liens de redirection -->
-        <div class="ml-3">
-            <div>
-                <h3><a href="https://vovinamworldfederation.eu/fr/">➢ Fédération Mondiale, Site Officiel</a></h3>
-            </div>
-            <br>
-            <div>
-                <h3><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/maitres-et-enseignants-internationaux.html">➢ Conseil des Maîtres Mondial</a></h3>
-            </div>
-            <br>
-            <div>
-                <h3><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/maitres-et-enseignants-internationaux/diplomes-internationaux.html">➢ Diplôme International</a></h3>
-            </div>
-            <br>
-            <br>
-            <div>
-                <h3><a href="http://www.famtv.fr/">➢ Fédération des Arts Martiaux Traditionnels Vietnamiens</a></h3>
-            </div>
-            
-            <br>
-            <div>
-                <h3><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/coupes-du-monde.html">➢ Résultats Coupes du Monde</a></h3>
-                <ul>
-                    <li>
-                        <h4><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/coupe-europe-2020.html">➢ Résultats Coupes d'Europe</a></h4>
-                    </li>
-                    <li>
-                        <h4><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/coupes-continentales.html">➢ Résultats Coupes d'Afrique</a></h4>
-                    </li>
-                </ul>
+        <div class="ml-3 d-flex justify-content-evenly">
+            <div class="Mondiale">
+                <div>
+                    <h3><a href="https://vovinamworldfederation.eu/fr/">➢ Fédération Mondiale, Site Officiel</a></h3>
+                </div>
+                <br>
+                <div>
+                    <h3><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/maitres-et-enseignants-internationaux.html">➢ Conseil des Maîtres Mondial</a></h3>
+                </div>
+                <br>
+                <div>
+                    <h3><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/maitres-et-enseignants-internationaux/diplomes-internationaux.html">➢ Diplôme International</a></h3>
+                </div>
+                <br>
+                
+                <div>
+                    <h3><a href="http://www.famtv.fr/">➢ Fédération des Arts Martiaux Traditionnels Vietnamiens</a></h3>
+                </div>
+            </div>        
+            <div class="result">
+                <br>
+                <div>
+                    <h3><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/coupes-du-monde.html">➢ Résultats Coupes du Monde</a></h3>
+                    <ul>
+                        <li>
+                            <h4><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/coupe-europe-2020.html">➢ Résultats Coupes d'Europe</a></h4>
+                        </li>
+                        <li>
+                            <h4><a href="https://vovinamworldfederation.eu/fr/conseil-mondial-des-maitres/coupes-continentales.html">➢ Résultats Coupes d'Afrique</a></h4>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
