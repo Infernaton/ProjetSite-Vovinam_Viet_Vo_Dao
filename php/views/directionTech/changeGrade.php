eba3dae573841adcd8891dfae6c7c69b77ba014b
Mathis <mathisouchikh@gmail.com>
2021-06-18 15:40:59 +0200

    Ajout de textes au passage de grade

--- php/views/directionTech/changeGrade.php
+++ php/views/directionTech/changeGrade.php
@@ -6,54 +6,35 @@
 </style>
 
 <div class="container">
-        <!-- Titre -->
-        <div class="text-center mt-5">
-            <h1 class="content-title-yellow">Passage de grades</h2>
-        </div>
-        <br>
-        <div class="row">
-
-            <img class="inter"src="assets/img/ceintures/Inter1.png">
-            <!-- Affichage du mannequin -->
-            <!--<div id="belt" class="col-sm-7">
-                <img style="width: 40%;" id="beltPicture" src="assets/img/ceintures/c_initiation.png">
-            </div>
-             Menu déroulant pour afficher n'importe quelle ceinture 
-            <div id="chooseBelt" class="col-sm-5">
-                <select name="belt" class="custom-select mb-3" onchange="document.getElementById('beltPicture').src = this.value">
-                    <option value="assets/img/ceintures/c_initiation.png" selected>Initiation</option>
-                    <option value="assets/img/ceintures/c_bleu_1er_cap.png">C. bleue 1er Cap</option>
-                    <option value="assets/img/ceintures/c_bleu_2eme_cap.png">C. bleue 2e Cap</option>
-                    <option value="assets/img/ceintures/c_bleu_3eme_cap.png">C. bleue 3e Cap</option>
-    
-                    <option value="assets/img/ceintures/CJ01.png">C. jaune 1er Dang</option>
-                    <option value="assets/img/ceintures/CJ02.png">C. jaune 2e Dang</option>
-                    <option value="assets/img/ceintures/CJ03.png">C. jaune 3e Dang</option>
-    
-                    <option value="assets/img/ceintures/CR4.png">C. rouge</option>
-                    <option value="assets/img/ceintures/CR5.png">C. rouge 1er degré</option>
-                    <option value="assets/img/ceintures/CR6.png">C. rouge 2e degré</option>
-                    <option value="assets/img/ceintures/CR7.png">C. rouge 3e degré</option>
-                    <option value="assets/img/ceintures/CR08.png">C. rouge 4e degré</option>
-                    <option value="assets/img/ceintures/CR09.png">C. rouge 5e degré</option>
-                    <option value="assets/img/ceintures/CR10.png">C. rouge 6e degré</option>
-
-                    <option value="assets/img/ceintures/CBlancheSupr.png">C. Blanche suprême</option>
-                </select>
-                <div class="text-center">
-                    <p>Pour toutes informations sur le Passage des différents grades :</p>
-                    <a href="assets/download_file/3.2.1_Les_grades_Internationaux.pdf">Voir "Les_grades_Internationaux.pdf"</a><br>
-                    <a href="assets/download_file/3.2.2_Les grades du Vovinam-Viàt Vo Dao.pdf">Voir "Les grades du Vovinam-Viàt Vo Dao.pdf"</a> <br>
-                    <a href="assets/download_file/3.3_Organisation des examens des grades.pdf">Voir "Organisation des examens des grades.pdf"</a> <br>
-                    <a href="assets/download_file/3.4_Programmes d'examens.pdf">Voir "Programmes d'examens.pdf"</a>
-                </div>
-                <div>
-                    <br>
-                    <br>
-                    <p class="border pl-3">
-                        Formulaire pour passer un grade: <a href="#" class="link">formulaire.pdf</a>
-                    </p>
-                </div>
-            </div>
-        </div>-->
+    <!-- Titre -->
+    <div class="text-center mt-5">
+        <h1 class="content-title-yellow">Passage de grades</h2>
     </div>
+    <br>
+    <div class="row">
+
+        <img class="inter"src="assets/img/ceintures/Inter1.png">
+        <div class="prgm">   
+            <p>
+                <h3>Programme type :</h3>
+                <br>
+                <h5>Nous donnons généralement rendez-vous à tous les candidats dès <strong>13h00</strong> pour l’appel, les vérifications et la pesée. 
+                <br>
+                <br>
+                Nous invitons tous les Maîtres participants au passage de grades à se présenter à <strong>13h30</strong> pour la réunion des membres du jury.
+                <br>
+                <br>
+                L’examen se déroulera habituellement de <strong>14h00 à 18h30</strong>14h00 à 18h30.
+                <br>
+                <br>
+                Afin de ne pas ajouter plus de stress à la préparation de cet examen, nous demandons de respecter précisément les démarches administratives suivantes :
+                <br>
+                <br>
+                <strong>Si vous souhaitez passer un grade n’hésitez pas revenir vers votre enseignant qui vous inscrira.</strong>
+                <br>
+                <br>
+                 
+            </p>
+        </div> 
+    </div>
+</div>
